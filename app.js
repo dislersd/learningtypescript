4e19d84caa9eb8cf6237123fbdcc151dec59592d
dislersd <dislersd@gmail.com>
2019-01-03 22:00:56 -0800

    added circle class

--- app.js
+++ app.js
@@ -4,14 +4,38 @@ function gameLoop() {
     requestAnimationFrame(gameLoop);
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, 1280, 720);
-    ctx.save();
-    ctx.beginPath();
-    ctx.strokeStyle = "red";
-    ctx.lineWidth = 5;
-    ctx.arc(400, 400, 100, 0, 2 * Math.PI);
-    ctx.stroke();
-    ctx.restore();
+    myCircle.draw();
+    myCircle2.draw();
 }
+var Circle = /** @class */ (function () {
+    function Circle(x, y, radius, color, line_width) {
+        if (color === void 0) { color = 'red'; }
+        if (line_width === void 0) { line_width = 2; }
+        var _this = this;
+        this.x = 0;
+        this.y = 0;
+        this.radius = 10;
+        this.lineWidth = 2;
+        this.color = 'red';
+        this.draw = function () {
+            ctx.save();
+            ctx.beginPath();
+            ctx.strokeStyle = _this.color;
+            ctx.lineWidth = _this.lineWidth;
+            ctx.arc(_this.x, _this.y, _this.radius, 0, 2 * Math.PI);
+            ctx.stroke();
+            ctx.restore();
+        };
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.lineWidth = line_width;
+    }
+    return Circle;
+}());
+var myCircle = new Circle(50, 75, 25);
+var myCircle2 = new Circle(200, 500, 50, 'blue');
 window.onload = function () {
     canvas = document.getElementById('cnvs');
     ctx = canvas.getContext("2d");
--- app.ts
+++ app.ts
@@ -6,14 +6,36 @@ function gameLoop() {
   ctx.fillStyle = "black";
   ctx.fillRect(0,0,1280,720);
 
-  ctx.save();
-  ctx.beginPath();
-  ctx.strokeStyle = "red";
-  ctx.lineWidth = 5;
-  ctx.arc(400,400,100,0,2*Math.PI);
-  ctx.stroke();
-  ctx.restore();
+  myCircle.draw();
+  myCircle2.draw();
 }
+
+class Circle {
+  public x: number = 0;
+  public y: number = 0;
+  public radius: number = 10;
+  public lineWidth: number = 2;
+  public color: string = 'red';
+  constructor(x: number, y: number, radius: number, color: string = 'red', line_width: number = 2) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.lineWidth = line_width;
+  }
+  public draw = (): void => {
+    ctx.save();
+    ctx.beginPath();
+    ctx.strokeStyle = this.color;
+    ctx.lineWidth = this.lineWidth;
+    ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.restore();
+  }
+}
+
+const myCircle: Circle = new Circle(50,75,25);
+const myCircle2: Circle = new Circle(200,500,50,'blue');
 
 window.onload = () => {
   canvas = <HTMLCanvasElement>document.getElementById('cnvs');
