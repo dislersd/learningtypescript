f5ec973dcbc78ea3dba2f291ac08ca047c52f0e5
dislersd <dislersd@gmail.com>
2019-01-03 23:05:31 -0800

    added asteroid class

--- app.js
+++ app.js
@@ -4,9 +4,44 @@ function gameLoop() {
     requestAnimationFrame(gameLoop);
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, 1280, 720);
-    myCircle.draw();
-    myCircle2.draw();
+    var shape;
+    for (var i = 0; i < shape_array.length; i++) {
+        shape = shape_array[i];
+        shape.draw();
+        shape.x++;
+    }
 }
+// ################################################ //
+var shape_array = new Array();
+var Rect = /** @class */ (function () {
+    function Rect(x, y, width, height, color, line_width) {
+        if (color === void 0) { color = 'blue'; }
+        if (line_width === void 0) { line_width = 5; }
+        var _this = this;
+        this.x = 0;
+        this.y = 0;
+        this.lineWidth = 5;
+        this.width = 0;
+        this.height = 0;
+        this.color = 'blue';
+        this.draw = function () {
+            ctx.save();
+            ctx.beginPath();
+            ctx.strokeStyle = _this.color;
+            ctx.lineWidth = _this.lineWidth;
+            ctx.rect(_this.x, _this.y, _this.width, _this.height);
+            ctx.stroke();
+            ctx.restore();
+        };
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        this.lineWidth = line_width;
+    }
+    return Rect;
+}());
 var Circle = /** @class */ (function () {
     function Circle(x, y, radius, color, line_width) {
         if (color === void 0) { color = 'red'; }
@@ -34,10 +69,67 @@ var Circle = /** @class */ (function () {
     }
     return Circle;
 }());
-var myCircle = new Circle(50, 75, 25);
-var myCircle2 = new Circle(200, 500, 50, 'blue');
+var Point = /** @class */ (function () {
+    function Point(x, y) {
+        this.x = 0;
+        this.y = 0;
+        this.x = x;
+        this.y = y;
+    }
+    return Point;
+}());
+var Asteroid = /** @class */ (function () {
+    function Asteroid(x, y, size, color, line_width) {
+        if (color === void 0) { color = 'white'; }
+        if (line_width === void 0) { line_width = 2; }
+        var _this = this;
+        this.x = 0;
+        this.y = 0;
+        this.lineWidth = 5;
+        this.color = 'white';
+        this.size = 20;
+        this.rotation = 0;
+        this.pointList = new Array();
+        this.draw = function () {
+            _this.rotation += 0.02;
+            ctx.save();
+            ctx.beginPath();
+            ctx.strokeStyle = _this.color;
+            ctx.lineWidth = _this.lineWidth;
+            ctx.rotate(_this.rotation);
+            ctx.moveTo(_this.x + _this.pointList[_this.pointList.length - 1].x, _this.y + _this.pointList[_this.pointList.length - 1].y);
+            for (var i = 0; i < _this.pointList.length; i++) {
+                ctx.lineTo(_this.x + _this.pointList[i].x, _this.y + _this.pointList[i].y);
+            }
+            ctx.closePath();
+            ctx.stroke();
+            ctx.restore();
+        };
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.pointList.push(new Point(0, 3 * size));
+        this.pointList.push(new Point(-1 * size, 2 * size));
+        this.pointList.push(new Point(-3.5 * size, size));
+        this.pointList.push(new Point(-3 * size, size));
+        this.pointList.push(new Point(-4 * size, 0));
+        this.pointList.push(new Point(2 * size, -4 * size));
+        this.pointList.push(new Point(3 * size, 1 * size));
+        this.pointList.push(new Point(4 * size, size));
+        this.pointList.push(new Point(size, 2 * size));
+        this.color = color;
+        this.lineWidth = line_width;
+    }
+    return Asteroid;
+}());
+// ################################################ //
 window.onload = function () {
     canvas = document.getElementById('cnvs');
     ctx = canvas.getContext("2d");
+    shape_array.push(new Circle(500, 500, 50));
+    shape_array.push(new Circle(70, 500, 20, '#fae'));
+    shape_array.push(new Rect(200, 200, 200, 100, 'white'));
+    shape_array.push(new Rect(700, 300, 200, 100, 'pink', 10));
+    shape_array.push(new Asteroid(850, 600, 20));
     gameLoop();
 };
--- app.ts
+++ app.ts
@@ -5,9 +5,52 @@ function gameLoop() {
   requestAnimationFrame(gameLoop);
   ctx.fillStyle = "black";
   ctx.fillRect(0,0,1280,720);
+  let shape: iShape;
+  for (let i: number = 0; i < shape_array.length; i++) {
+  shape = shape_array[i];
+  shape.draw();
+  shape.x++
+  }
 
-  myCircle.draw();
-  myCircle2.draw();
+}
+
+// ################################################ //
+
+let shape_array: Array<iShape> = new Array<iShape>();
+
+interface iShape {
+  draw(): void;
+  x: number;
+  y: number;
+  color: string;
+  lineWidth: number;
+}
+
+class Rect implements iShape {
+  public x: number = 0;
+  public y: number = 0;
+  public lineWidth: number = 5;
+  public width: number = 0;
+  public height: number = 0;
+  public color: string = 'blue';
+
+  constructor(x: number, y: number, width: number, height: number, color: string = 'blue', line_width: number = 5) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+    this.lineWidth = line_width;
+  }
+  public draw = (): void => {
+    ctx.save();
+    ctx.beginPath();
+    ctx.strokeStyle = this.color;
+    ctx.lineWidth = this.lineWidth;
+    ctx.rect(this.x, this.y, this.width, this.height);
+    ctx.stroke();
+    ctx.restore();
+  }
 }
 
 class Circle {
@@ -34,11 +77,77 @@ class Circle {
   }
 }
 
-const myCircle: Circle = new Circle(50,75,25);
-const myCircle2: Circle = new Circle(200,500,50,'blue');
+class Point {
+  public x: number = 0;
+  public y: number = 0;
+
+  constructor(x:number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+class Asteroid implements iShape {
+  public x: number = 0;
+  public y: number = 0;
+  public lineWidth: number = 5;
+  public color: string = 'white';
+  public size: number = 20;
+  public rotation: number = 0;
+  public pointList: Array<Point> = new Array<Point>();
+
+  public draw = (): void => {
+    this.rotation += 0.02;
+    ctx.save();
+    ctx.beginPath();
+    ctx.strokeStyle = this.color;
+    ctx.lineWidth = this.lineWidth;
+    ctx.rotate(this.rotation);
+
+    ctx.moveTo(this.x + this.pointList[this.pointList.length - 1].x, this.y + this.pointList[this.pointList.length - 1].y);
+
+    for (var i: number = 0; i < this.pointList.length; i++) {
+      ctx.lineTo(this.x + this.pointList[i].x, this.y + this.pointList[i].y);
+    }
+
+    ctx.closePath();
+    ctx.stroke();
+    ctx.restore();
+  }
+
+  constructor(x: number, y: number, size: number, color: string = 'white', line_width: number = 2) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+
+    this.pointList.push(new Point(0, 3 * size));
+    this.pointList.push(new Point(-1 * size, 2 * size));
+    this.pointList.push(new Point(-3.5 * size, size));
+    this.pointList.push(new Point(-3 * size, size));
+    this.pointList.push(new Point(-4 * size, 0));
+    this.pointList.push(new Point(2 * size, -4 * size));
+    this.pointList.push(new Point(3 * size, 1 * size));
+    this.pointList.push(new Point(4 * size, size));
+    this.pointList.push(new Point(size, 2 * size));
+
+    this.color = color;
+    this.lineWidth = line_width;
+    
+  }
+}
+
+
+// ################################################ //
 
 window.onload = () => {
   canvas = <HTMLCanvasElement>document.getElementById('cnvs');
   ctx = canvas.getContext("2d");
+
+  shape_array.push(new Circle(500,500,50));
+  shape_array.push(new Circle(70,500,20, '#fae'));
+  shape_array.push(new Rect(200,200,200,100,'white'));
+  shape_array.push(new Rect(700,300,200,100,'pink', 10));
+  shape_array.push(new Asteroid(850,600,20));
+
   gameLoop();
 }
